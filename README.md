0c11fe870f340627d9f9e19dc3159798b9799ff7
Matt <msb@ameritech.net>
2023-01-05 16:58:27 -0500

    Implement basic todo functions

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # productivity-app
 An app to manage projects and tasks, and boost overall productivity
+
+## Actions
+### Current
+* Update "Add Project..." Button
+* Create functionality for "Add Task"
+  - Control focus when tab is pressed
+
+* Save data in JSON for future use
+
+### Future
+* Add status
+* Add sch date
--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,144 @@
-function addProject() {
-  const prjList = document.querySelector('.projects-list');
+let newProjectID = 2;
 
-  // Create new info
-  let newPrjDiv = document.createElement('div');
-  newPrjDiv.classList.add('project');
-  newPrjDiv.id = 'prj-2';
+function newElement(selector) {
+  let elClass = "";
+  let elID = "";
 
-  let newPrjTitle = document.createElement('div');
-  newPrjTitle.classList.add('prj-title');
-  newPrjTitle.textContent = "SQL";
-  newPrjDiv.appendChild(newPrjTitle);
+  if (selector.includes('.')) {
+    const classArray = selector.split(".");
+    elClass = classArray[1];
+    selector = classArray[0];
+  } 
+  if (selector.includes('#')) {
+    const classArray = selector.split("#");
+    elID = classArray[1];
+    selector = classArray[0];
+  }
 
-  let newPrjTaskList = document.createElement('div');
-  newPrjTitle.classList.add('prj-tasks');
-
-  let newUL = document.createElement('ul');
-
-  let newLI = document.createElement('li');
-  newLI.textContent = "Next lesson";
-  newUL.appendChild(newLI);
-
-  newLI = document.createElement('li');
-  let newAddButton = document.createElement('input');
-  newAddButton.setAttribute('type', 'text');
-  newAddButton.setAttribute('placeholder', 'Add a task...');
-  newAddButton.classList.add('new-task');
-  newLI.appendChild(newAddButton);
-  newUL.appendChild(newLI);
-
-  // Bubble up
-  newPrjTaskList.appendChild(newUL);
-  newPrjDiv.appendChild(newPrjTaskList);
-  prjList.appendChild(newPrjDiv);
+  const newEl = document.createElement(selector);
+  if (elClass) newEl.classList.add(elClass);
+  if (elID) newEl.id = elID;
+  
+  return newEl;
 }
 
-const addButton = document.querySelector('.new-project');
-addButton.addEventListener('click', addProject);
+function addProject(e) {
+  if (!(e.key == "Enter" && e.target.value)) return;
+
+  const prjList = document.querySelector('.projects-list');
+  const newProject = createProject(e.target.value);
+  prjList.appendChild(newProject);
+  e.target.value = "";
+
+  function createProject(title) {
+    // Create project div, add class & ID
+    const newPrjDiv = newElement('div.project');
+    newPrjDiv.id = "prj-" + newProjectID.toString();
+
+    const newPrjTitle = createProjectTitle(title, newProjectID);
+    newPrjDiv.appendChild(newPrjTitle);
+
+    // Add blank task list
+    const taskList = createTaskList();
+    newPrjDiv.appendChild(taskList);
+
+    // Return to main function
+    newProjectID++; // Iterate to next ID for next new project
+    return newPrjDiv;
+  }
+
+  function createProjectTitle(title, prjID) {
+    const newPrjTitle = newElement('div.prj-title');
+
+    const deleteBtn = newElement('img.delete-project');
+    deleteBtn.setAttribute('src', './img/delete.svg');
+    deleteBtn.id = "del-btn-" + prjID.toString();
+    deleteBtn.addEventListener('click', deleteProject);
+    newPrjTitle.appendChild(deleteBtn);
+
+    newPrjTitleH3 = document.createElement('h3');
+    newPrjTitleH3.textContent = title;
+    newPrjTitle.appendChild(newPrjTitleH3);
+
+    return newPrjTitle;
+  }
+
+  function createTaskList() {
+    // Create element, add class
+    const newPrjTaskList = newElement('div.prj-tasks');
+
+    const newUL = createProjectUL();
+    newPrjTaskList.appendChild(newUL);
+
+    return newPrjTaskList;
+  }
+
+  function createProjectUL() {
+    const newUL = document.createElement('ul');
+    const newLI = document.createElement('li');
+    newLI.classList.add('new-task');
+
+    const newInput = document.createElement('input');
+    newInput.setAttribute('type', 'text');
+    newInput.setAttribute('placeholder', 'Add a task...');
+    newInput.addEventListener('keydown', addItem);
+
+    newLI.appendChild(newInput);
+    newUL.appendChild(newLI);
+
+    return newUL;
+  }
+}
+
+function deleteProject(e) {
+  const projectDiv = e.target.parentNode.parentNode;
+  projectDiv.remove();
+}
+
+function addItem(e) {
+  if ((e.key == "Enter") && e.target.value) {
+    let newLI = document.createElement('li');
+    newLI.textContent = e.target.value;
+    newLI.addEventListener('click', toggleComplete);
+    e.target.parentNode.parentNode.insertBefore(newLI, e.target.parentNode);
+    e.target.value = "";
+  } 
+}
+
+function toggleComplete(e) {
+  e.target.classList.toggle("task-complete"); 
+}
+
+function clearCompleteTasks(e) {
+  let tasksComplete = document.querySelectorAll('.task-complete');
+  Array.from(tasksComplete).map(el => el.remove());
+  // console.log(tasksComplete);
+}
+
+function addFunctions() {
+  // Add function to "Add Projects" button
+  // const addButton = document.querySelector('.new-project');
+  // addButton.addEventListener('click', addProject);
+  const addPrjInput = document.querySelector('#new-project');
+  addPrjInput.addEventListener('keydown', addProject);
+
+  // Respond to enter/tab on input box
+  const myInput = document.querySelector('input');
+  myInput.addEventListener('keydown', addItem);
+
+  const liList = document.querySelectorAll('li');
+
+  for (let i=0; i<liList.length; i++) {
+    if (liList[i].classList.contains('new-task')) continue;
+    liList[i].addEventListener('click', toggleComplete);
+  }
+
+  const clearBtn = document.querySelector('#clear-complete');
+  clearBtn.addEventListener('click', clearCompleteTasks);
+
+  const delBtn1 = document.querySelector('#del-btn-1')
+  delBtn1.addEventListener('click', deleteProject);
+}
+
+
+addFunctions();
